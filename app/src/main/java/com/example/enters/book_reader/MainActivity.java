6260ed2e3eff5a75c414a2f08d451e54b4ef89c8
Yashvida Jayasekara <yashvida.1007@gmail.com>
2018-03-29 22:05:43 +0530

    Added Loading and Logo

---
+++ app/src/main/java/com/example/enters/book_reader/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.enters.book_reader;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntend = new Intent(MainActivity.this, SearchBookActivity.class);
+                startActivity(homeIntend);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
