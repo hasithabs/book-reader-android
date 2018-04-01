8e13d451a76ed5d812c02287890cad6b68407b49
Hasitha Bhagya Seneviratne <hasitha.bigwig@gmail.com>
2018-04-01 08:28:13 +0530

    Add - Option menu with table create, drop

--- app/src/main/java/com/example/enters/book_reader/UI/HomeActivity.java
+++ app/src/main/java/com/example/enters/book_reader/UI/HomeActivity.java
@@ -9,13 +9,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.enters.book_reader.Helper.DatabaseHelper;
 import com.example.enters.book_reader.R;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    DatabaseHelper bookDB;
     private CardView searchCard;
     private CardView favoriteCard;
+
+    private int groupMenuId = 1;
+
+    int createId = Menu.FIRST;
+    int dropId = Menu.FIRST +1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         favoriteCard =(CardView) findViewById(R.id.cardViewFavorite);
         searchCard.setOnClickListener(this);
         favoriteCard.setOnClickListener(this);
+        bookDB = DatabaseHelper.getDbInstance(this);
     }
 
     @Override
@@ -40,4 +49,30 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(groupMenuId, createId, createId, "Add Table");
+        menu.add(groupMenuId, dropId, dropId, "Drop Table");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                bookDB.createTable();
+                bookDB.insertData();
+                Toast.makeText(this, "Create table & add data", Toast.LENGTH_LONG);
+                return true;
+
+            case 2:
+                bookDB.dropTable();
+                Toast.makeText(this, "Drop table", Toast.LENGTH_LONG);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
